11655fd2a1998af410fb23d2236fc77bacf9da76
yusuphutubaste <yusuphutubaste@gmail.com>
2026-08-06 14:56:13 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav");
+
+
+hamburger.addEventListener("click", () => {
+
+    hamburger.classList.toggle("active");
+
+    navMenu.classList.toggle("active");
+
+});
